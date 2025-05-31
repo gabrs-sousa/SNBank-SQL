b029f75329c4cf21a978f59062ea3311b72b0403
Gabriel Sousa <49318376+gabrs-sousa@users.noreply.github.com>
2025-05-31 16:01:33 -0300

    Banco de Dados com SN Bank
    As instruções são necessárias para criar o modelo físico do projeto SN Bank

---
+++ ALTER_SNBank.sql
@@ -0,0 +1,8 @@
+DROP TABLE tabelaclienteconta;
+DROP TABLE tabelaclientes;
+DROP TABLE tabelacolaboradores;
+DROP TABLE tabelaconta;
+DROP TABLE tabeladepartamento;
+DROP TABLE tabelaemprestimos;
+DROP TABLE tabelapagamento;
+DROP TABLE tabelascorecredito;
