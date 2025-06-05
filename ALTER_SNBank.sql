85fc1227caeb600603227878756df9ef695ede79
Gabriel Sousa <49318376+gabrs-sousa@users.noreply.github.com>
2025-06-05 11:17:48 -0300

    Organizando

--- ALTER_SNBank.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE tabelaclienteconta;
-DROP TABLE tabelaclientes;
-DROP TABLE tabelacolaboradores;
-DROP TABLE tabelaconta;
-DROP TABLE tabeladepartamento;
-DROP TABLE tabelaemprestimos;
-DROP TABLE tabelapagamento;
-DROP TABLE tabelascorecredito;
